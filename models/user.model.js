a28fe31ffe5bde2a6949d3e0e571e1173ba8f40e
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-06-06 23:15:01 +0600

    user model is complteed

--- models/user.model.js
+++ models/user.model.js
@@ -1,20 +1,119 @@
+const { text } = require("express");
 const mongoose = require("mongoose");
-const { schema } = mongoose;
+const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
       required: true,
+      trim: true,
+      text: true,
     },
     lastName: {
       type: String,
       required: true,
+      trim: true,
+      text: true,
     },
     email: {
       type: String,
       required: true,
+      trim: true,
     },
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      text: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    cover: {
+      type: String,
+      default: "",
+    },
+    birthDate: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    request: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    search: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+          text: true,
+        },
+        createdAt: {
+          type: Date,
+          require: true,
+        },
+      },
+    ],
+    details: {
+      currentCity: {
+        type: String,
+      },
+      workplace: {
+        type: String,
+      },
+      university: {
+        type: String,
+      },
+      hometown: {
+        type: String,
+      },
+    },
+    savedPost: [
+      {
+        post: {
+          type: Schema.Types.ObjectId,
+          ref: "Post",
+        },
+        savedAt: {
+          type: Date,
+          require: true,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
